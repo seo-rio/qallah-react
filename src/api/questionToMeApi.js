15be786589fd2cf2bc6d070092d25e770f2661cc
서충식 <724584@naver.com>
2020-11-11 01:23:10 +0900

    Api 모듈화 사전 단계

---
+++ src/api/questionToMeApi.js
@@ -0,0 +1,27 @@
+import axios from 'axios';
+import apiConfing from './apiConfig';
+
+// // question 데이터 조회
+// export function fetchQuestion() {
+//     return axios.get('/getData');
+// }
+
+// // question 생성
+// export function createQuestion() {
+//     return axios.get('');
+// }
+
+export const questionToMeApi = {
+    getQuestionList() {
+        return apiConfing({
+            url: '/getData',
+            method: 'get',
+        });
+    },
+    registerAnswer() {
+        return apiConfing({
+            url: '/registeranswer',
+            method: 'post',
+        });
+    },
+};
--- src/pages/Home.js
+++ src/pages/Home.js
@@ -1,6 +1,7 @@
 import React, { useState, useEffect } from 'react';
 import axios from 'axios';
-import * as service from '../api/posts';
+import * as service from '../api/questionToMeApi';
+import { questionToMeApi } from '../api/questionToMeApi';
 
 const Home = () => {
     const [username, setUserName] = useState(null);
@@ -18,28 +19,39 @@ const Home = () => {
         // // }).catch(function(error){
         // //     console.log(error);
         // // })
-        axios.get('/getData', {
-        }).then ( function (res) {
-            console.log(res)
-        }).catch( function (err) {
-            console.log(err);
-        })
-        console.log('Hello')
+        // axios.get('/getData', {
+        // }).then ( function (res) {
+        //     console.log(res)
+        // }).catch( function (err) {
+        //     console.log(err);
+        // })
+        // console.log('Hello')
+        questionToMeApi
+            .getQuestionList()
+            .then(function (res) {
+                console.log(res);
+            })
+            .catch(function (err) {
+                console.log(err);
+            });
     };
-    
+
     useEffect(() => {
-        fetch('/api/getUsername').then(res=>res.json())
-        .then(user=> setUserName(user.username))
+        fetch('/api/getUsername')
+            .then((res) => res.json())
+            .then((user) => setUserName(user.username));
     }, []);
 
     return (
         <div>
             <h1>홈</h1>
             <p>이곳은 홈이에요.</p>
-            {username ? <h1>{`Hello ${username}`}</h1>: <h1>Loading... please wait</h1>}
-            <button onClick={() => clickEvent()}>
-                버튼 눌러주세요.
-            </button>
+            {username ? (
+                <h1>{`Hello ${username}`}</h1>
+            ) : (
+                <h1>Loading... please wait</h1>
+            )}
+            <button onClick={() => clickEvent()}>버튼 눌러주세요.</button>
         </div>
     );
 };
